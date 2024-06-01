6b06296955c3c589152a56f1174973ab73c011e9
Jakub Bazelak <jakubbazelak@gmail.com>
2024-06-01 10:42:35 +0200

    poprawiono gre w oczko

--- zrobione/gra_w_oczko.py
+++ zrobione/gra_w_oczko.py
@@ -11,16 +11,18 @@ def play_game():
             print("Tura gracza:")
             # Gracz wybiera wartość żetonu (4, 5 lub 6)
             player_choice = int(input("Wybierz wartość żetonu (4, 5 lub 6): "))
+            while player_choice not in [4, 5, 6]:
+                print("Niewłaściwy wybór! Wybierz wartość żetonu (4, 5 lub 6): ")
+                player_choice = int(input("Wybierz wartość żetonu (4, 5 lub 6): "))
             table_sum += player_choice
             player_turn = False
         else:
             print("Tura komputera:")
-            # Komputer wybiera wartość żetonu (4, 5 lub 6)
-            computer_choice = min(4, 5, 6, key=lambda x: minmax(table_sum + x, True))
+            # Komputer wybiera wartość żetonu (4, 5 lub 6) losowo
+            computer_choice = random.choice([4, 5, 6])
             table_sum += computer_choice
+            print("Komputer dołożył żeton o wartości:", computer_choice)
             player_turn = True
-
-        print("Gracz dołożył żeton o wartości:", player_choice if player_turn else computer_choice)
 
         # Sprawdź warunki końcowe gry
         if table_sum > 21:
@@ -29,25 +31,6 @@ def play_game():
         elif table_sum == 21:
             print("Suma na stole wynosi dokładnie 21. Remis.")
             break
-
-def minmax(table_sum, is_maximizing):
-    if table_sum > 21:
-        return -1 if is_maximizing else 1
-    elif table_sum == 21:
-        return 0
-
-    if is_maximizing:
-        best_value = float('-inf')
-        for choice in [4, 5, 6]:
-            value = minmax(table_sum + choice, False)
-            best_value = max(best_value, value)
-        return best_value
-    else:
-        best_value = float('inf')
-        for choice in [4, 5, 6]:
-            value = minmax(table_sum + choice, True)
-            best_value = min(best_value, value)
-        return best_value
 
 # Rozpocznij grę
 play_game()
